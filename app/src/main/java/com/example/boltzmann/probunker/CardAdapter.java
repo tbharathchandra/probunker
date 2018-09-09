ddd15caabc80b7b0de8069c35f79ef2656c9030d
tbharathchandra <t.bharathchandra@gmail.com>
2018-09-09 13:49:23 +0530

    all button funtionalities are added

--- app/src/main/java/com/example/boltzmann/probunker/CardAdapter.java
+++ app/src/main/java/com/example/boltzmann/probunker/CardAdapter.java
@@ -7,14 +7,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
     private Context mContext;
     private Cursor mCursor;
-    public CardAdapter(Context context, Cursor cursor){
+    private butttonsAdapetrListener mlistener;
+    public CardAdapter(Context context, Cursor cursor ,butttonsAdapetrListener listener){
         mContext=context;
         mCursor=cursor;
+        mlistener=listener;
     }
 
     @NonNull
@@ -34,10 +37,16 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
         int total = mCursor.getInt(mCursor.getColumnIndex("TOTAL"));
         int bunked = mCursor.getInt(mCursor.getColumnIndex("BUNK"));
         int color = mCursor.getInt(mCursor.getColumnIndex("COLOR"));
+        int id= mCursor.getInt(mCursor.getColumnIndex("_id"));
         holder.nameText.setText(name);
         holder.totalText.setText(Integer.toString(total));
         holder.bunkedText.setText(Integer.toString(bunked));
         holder.colorText.setBackgroundColor(color);
+        holder.delete.setTag(id);
+        holder.plusTotal.setTag(id);
+        holder.plusBunk.setTag(id);
+        holder.minusTotal.setTag(id);
+        holder.minusBunk.setTag(id);
     }
 
     @Override
@@ -50,13 +59,65 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
         public TextView totalText;
         public TextView bunkedText;
         public TextView colorText;
+        public Button delete;
+        public Button plusTotal;
+        public Button minusTotal;
+        public Button plusBunk;
+        public Button minusBunk;
         public CardViewHolder(@NonNull View itemView) {
             super(itemView);
             nameText= itemView.findViewById(R.id.name);
             totalText = itemView.findViewById(R.id.total_num);
             bunkedText = itemView.findViewById(R.id.bunked_num);
             colorText= itemView.findViewById(R.id.color);
+            delete= itemView.findViewById(R.id.delete);
+            plusTotal= itemView.findViewById(R.id.plus_total);
+            minusTotal=itemView.findViewById(R.id.minus_total);
+            plusBunk = itemView.findViewById(R.id.plus_bunk);
+            minusBunk = itemView.findViewById(R.id.minus_bunk);
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id=(int) view.getTag();
+                    mlistener.deleteOnClick(id,getAdapterPosition());
+                }
+            });
+            plusTotal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id=(int) view.getTag();
+                    mlistener.plusTotalOnClick(id,getAdapterPosition());
+                }
+            });
+            minusTotal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id = (int) view.getTag();
+                    mlistener.minusTotalOnClick(id,getAdapterPosition());
+                }
+            });
+            plusBunk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id= (int) view.getTag();
+                    mlistener.plusBunkOnClick(id,getAdapterPosition());
+                }
+            });
+            minusBunk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id= (int) view.getTag();
+                    mlistener.minusBunkOnClick(id,getAdapterPosition());
+                }
+            });
         }
+    }
+    public interface butttonsAdapetrListener{
+        void deleteOnClick(int id,long adapterpos);
+        void plusTotalOnClick(int id,long adapterpos);
+        void minusTotalOnClick(int id,long adapterpos);
+        void plusBunkOnClick(int id,long adapterpos);
+        void minusBunkOnClick(int id,long adapterpos);
     }
     public void swapCursor(Cursor newCursor){
         if(mCursor!=null){
--- app/src/main/java/com/example/boltzmann/probunker/MainActivity.java
+++ app/src/main/java/com/example/boltzmann/probunker/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.boltzmann.probunker;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,10 +26,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SQLiteOpenHelper proBunkerDatabaseHelper = new ProBunkerDatabaseHelper(this);
-        db = proBunkerDatabaseHelper.getReadableDatabase();
+        db = proBunkerDatabaseHelper.getWritableDatabase();
         RecyclerView recyclerView =  findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new CardAdapter(this,getCursor());
+        mAdapter = new CardAdapter(this, getCursor(), new CardAdapter.butttonsAdapetrListener() {
+            @Override
+            public void deleteOnClick(int id,long position) {
+                dailog(id);
+            }
+
+            @Override
+            public void plusTotalOnClick(int id, long adapterpos) {
+                Cursor data = db.query("MYTABLE",new String[]{"TOTAL"},"_id=?",new String[]{Integer.toString(id)},null,null,null);
+                data.moveToFirst();
+                int total = data.getInt(data.getColumnIndex("TOTAL"));
+                total=total+1;
+                ContentValues values = new ContentValues();
+                values.put("TOTAL",total);
+                db.update("MYTABLE",values,"_id=?",new String[]{Integer.toString(id)});
+                data.close();
+                mAdapter.swapCursor(getCursor());
+            }
+
+            @Override
+            public void minusTotalOnClick(int id, long adapterpos) {
+                Cursor data = db.query("MYTABLE",new String[]{"TOTAL"},"_id=?",new String[]{Integer.toString(id)},null,null,null);
+                data.moveToFirst();
+                int total = data.getInt(data.getColumnIndex("TOTAL"));
+                if(total>0){
+                    total=total-1;
+                    ContentValues values = new ContentValues();
+                    values.put("TOTAL",total);
+                    db.update("MYTABLE",values,"_id=?",new String[]{Integer.toString(id)});
+                    data.close();
+                    mAdapter.swapCursor(getCursor());
+                }else{
+                    Toast toast =Toast.makeText(MainActivity.this,"total classes are already zero",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+
+            @Override
+            public void plusBunkOnClick(int id, long adapterpos) {
+                Cursor data = db.query("MYTABLE",new String[]{"BUNK"},"_id=?",new String[]{Integer.toString(id)},null,null,null);
+                data.moveToFirst();
+                int bunk = data.getInt(data.getColumnIndex("BUNK"));
+                bunk = bunk+1;
+                ContentValues values = new ContentValues();
+                values.put("BUNK",bunk);
+                db.update("MYTABLE",values,"_id=?",new String[]{Integer.toString(id)});
+                data.close();
+                mAdapter.swapCursor(getCursor());
+            }
+
+            @Override
+            public void minusBunkOnClick(int id, long adapterpos) {
+                Cursor data = db.query("MYTABLE",new String[]{"BUNK"},"_id=?",new String[]{Integer.toString(id)},null,null,null);
+                data.moveToFirst();
+                int bunk = data.getInt(data.getColumnIndex("BUNK"));
+                if(bunk>0){
+                    bunk = bunk-1;
+                    ContentValues values = new ContentValues();
+                    values.put("BUNK",bunk);
+                    db.update("MYTABLE",values,"_id=?",new String[]{Integer.toString(id)});
+                    data.close();
+                    mAdapter.swapCursor(getCursor());
+                }else{
+                    Toast toast = Toast.makeText(MainActivity.this,"buked classes are already zero",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
         recyclerView.setAdapter(mAdapter);
         FloatingActionButton b = findViewById(R.id.fab);
         b.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +110,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public Cursor getCursor(){
-        return db.query("MYTABLE",null,null,null,null,null,null);
+        return db.query("MYTABLE",null,null,null,null,null,"_id ASC");
+    }
+    public void dailog(final int id){
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+        a_builder.setMessage("Do you want to delete this subject?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        db.delete("MYTABLE","_id="+id,null);
+                        mAdapter.swapCursor(getCursor());
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Alert!!");
+        alert.show();
     }
 }
